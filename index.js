5d9dd96a324d1e5233459c9d8d6cb9308f24eb4e
Benoît Zugmeyer <bzugmeyer@gmail.com>
2015-10-19 19:39:49 +0200

    Initial api

---
+++ index.js
@@ -0,0 +1,128 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const parseArgs = require("minimist");
+const bodyParser = require("body-parser");
+const gpg = require("gpg");
+const PromiseUtil = require("./promise-util");
+const fileStat = PromiseUtil.wrapCPS(fs.stat);
+const fileRead = PromiseUtil.wrapCPS(fs.readFile);
+const directoryRead = PromiseUtil.wrapCPS(fs.readdir);
+const clearsign = PromiseUtil.wrapCPS(gpg.clearsign);
+
+class InvalidParameter extends Error {}
+class GPGError extends Error {
+    constructor(error) {
+        const message = error.message
+            .replace(/^gpg:\s*/, "")
+            .replace(/\n.*/g, "");
+        super(message);
+    }
+}
+
+const listDirectory = PromiseUtil.wrapRun(function* (root, filter) {
+    const files = yield directoryRead(root);
+
+    const result = yield files.map(PromiseUtil.wrapRun(function* (name) {
+        const filePath = path.join(root, name);
+        const stat = yield fileStat(filePath);
+        if (!filter || filter(name, stat)) {
+            return stat.isDirectory() ? {
+                name,
+                children: yield listDirectory(filePath),
+            } : {
+                name,
+            };
+        }
+    }));
+
+    return result.filter((file) => file);
+});
+
+function filterFiles(name, stat) {
+    return (stat.isDirectory() && name !== ".git") || name.endsWith(".gpg");
+}
+
+const auth = PromiseUtil.wrapRun(function* (conf, passphrase) {
+    try {
+        yield clearsign("foo", ["--pinentry-mode", "loopback", "--passphrase", passphrase, "--quiet", "--local-user", conf.gpgId]);
+    }
+    catch (e) {
+        throw new GPGError(e);
+    }
+});
+
+function apiRouter(conf) {
+    const router = express.Router();
+
+    router.use(bodyParser.json());
+
+    function sendError(res, error) {
+        res.json({
+            error: {
+                type: error.constructor.name,
+                message: error.message,
+            },
+        });
+    }
+
+    function wrap(gen) {
+        return PromiseUtil.wrapRun(function* (req, res, next) {
+            try {
+                yield* gen(req, res, next);
+            }
+            catch (error) {
+                sendError(res, error);
+            }
+        });
+    }
+
+    router.use(wrap(function* (req, res, next) {
+        if (!req.body) throw new InvalidParameter("No request body");
+        if (!req.body.passphrase) throw new InvalidParameter("No passphrase");
+        yield auth(conf, req.body.passphrase);
+        next();
+    }));
+
+    router.post("/list", wrap(function* (req, res) {
+        res.json(yield listDirectory(conf.passwordStorePath, filterFiles));
+    }));
+
+    return router;
+}
+
+function launchApp(conf) {
+    const app = express();
+
+    app.use(express.static("dist"));
+    app.use("/api", apiRouter(conf));
+
+    app.listen(3000, "localhost", function () {
+        const address = this.address();
+        console.log(`Server listening on http://${address.address}:${address.port}`);
+    });
+}
+
+const args = parseArgs(process.argv, {
+    alias: {
+        debug: [ "d" ],
+        store: [ "s" ],
+    },
+});
+
+PromiseUtil.run(function* () {
+    const passwordStorePath = args.store || path.join(process.env.HOME, ".password-store");
+    const passwordStoreStat = yield fileStat(passwordStorePath);
+    if (!passwordStoreStat.isDirectory()) throw new Error(`${passwordStorePath} is not a directory`);
+
+    const gpgId = yield fileRead(path.join(passwordStorePath, ".gpg-id"), { encoding: "utf-8" });
+
+    launchApp({
+        debug: args.debug,
+        passwordStorePath,
+        gpgId: gpgId.trim(),
+    });
+})
+.catch((e) => console.log(args.debug ? e.stack : e.message));
---
+++ promise-util.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const castPromise = (object) =>
+  object && typeof object.then === "function" ? object : Promise.resolve(object);
+
+const runIterator = (iterator) =>
+  new Promise((resolve, reject) => {
+    const iterate = (next, value) => {
+      let ret;
+      try {
+        ret = next ? iterator.next(value) : iterator.throw(value);
+      }
+      catch (error) {
+        reject(error);
+        return;
+      }
+
+      if (ret.done) {
+        resolve(ret.value);
+        return;
+      }
+
+      const promise = Array.isArray(ret.value) ? Promise.all(ret.value) : Promise.resolve(ret.value);
+      promise.then(
+        (value) => iterate(true, value),
+        (error) => iterate(false, error)
+      );
+    };
+
+    iterate(true);
+  });
+
+const run = (generator) => runIterator(generator());
+
+const wrapRun = (generator) =>
+  function () {
+    return runIterator(generator.apply(this, arguments));
+  };
+
+const wrapCPS = (fn, options) =>
+  function () {
+    const args = Array.from(arguments);
+    return new Promise((resolve, reject) => {
+      args.push(function () {
+        const resultArgs = Array.from(arguments);
+        const error = options && options.noError ? null : resultArgs.shift();
+
+        if (error) reject(error);
+        else if (options && options.multi) {
+          const result = {};
+          for (let i = 0; i < options.multi.length; i++) {
+            result[options.multi[i]] = resultArgs[i];
+          }
+          resolve(result);
+        }
+        else resolve(resultArgs[0]);
+      });
+      fn.apply(null, args);
+    });
+  };
+
+module.exports = {
+  wrapCPS,
+  run,
+  wrapRun,
+  castPromise,
+};
