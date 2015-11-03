a7651f3024765b184f99add7036af81bc1965ef2
Benoît Zugmeyer <bzugmeyer@gmail.com>
2015-11-03 21:33:20 +0100

    Better handling of HTTP errors

--- public/actions.js
+++ public/actions.js
@@ -4,7 +4,21 @@ import { emptyClipboard } from "./selection";
 
 function call(route, data) {
   m.redraw();
-  return m.request({ method: "POST", url: "api/" + route, data })
+  return m.request({
+    method: "POST",
+    url: "api/" + route,
+    data,
+    config(xhr) {
+      xhr.responseType = "json";
+    },
+    deserialize(i) {
+      return i;
+    },
+    extract(xhr) {
+      if (xhr.status !== 200) throw new Error(`Unexpected HTTP status code ${xhr.status}`);
+      return xhr.response;
+    },
+  })
   .then((response) => {
     if (response.error) {
       throw Object.assign(new Error(response.error.message), response.error);
